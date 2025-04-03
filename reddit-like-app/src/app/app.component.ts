f9a41aae261a240f23f2d1bc41da99dc6b7ddcd3
mattaLeo <matta.leonardo@iisgalvanimi.edu.it>
2025-04-03 08:54:35 +0000

    commit 3

--- reddit-like-app/src/app/app.component.ts
+++ reddit-like-app/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ArticleComponent } from './article/article.component';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-root',
@@ -10,6 +11,17 @@ import { ArticleComponent } from './article/article.component';
 })
 export class AppComponent {
   title = 'reddit-like-app';
+
+  articles:Article[];   // <-- component property
+  constructor(){
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
+  }
+
+
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
     return false;
